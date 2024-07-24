447ca428b87b354704cc9b3d5fb417c7a9064f77
Sachin Kumar <stdtech7878@gmail.com>
2024-07-24 18:33:11 +0530

    Partners section in home page is craeted

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -1,29 +1,21 @@
-import React from 'react'
-import Video from "../../assets/videos/world.mp4"
+import React from 'react';
+import Video from "../../assets/videos/world.mp4";
 
 export default function HeroSection() {
     return (
-        <header
-            class="relative flex items-center justify-center h-screen mb-12 overflow-hidden"
-        >
-            <div
-                class="relative z-30 p-5 text-7xl text-[var(--secondary-color)] bg-opacity-50 rounded-xl font-bold"
-            >
+        <section className="relative flex items-center h-screen justify-center overflow-hidden">
+            <div className="relative z-30 p-5 text-7xl text-[var(--secondary-color)] bg-opacity-50 rounded-xl font-bold">
                 Welcome to <span className='text-[var(--main-color)]'>Chandra <br /> Krishna</span> Creative
             </div>
             <video
-                autoplay
+                autoPlay
                 loop
-                muted={false}
-                controls={false}
-                class="absolute z-10 w-auto min-w-full min-h-full max-w-none brightness-75"
+                muted
+                className="absolute object-cover z-10 h-full w-screen max-w-none brightness-75"
             >
-                <source
-                    src={Video}
-                    type="video/mp4"
-                />
-                Your browser does not support the video tag.
+                <source src={Video} type="video/mp4" />
+                {/* Your browser does not support the video tag. */}
             </video>
-        </header>
-    )
+        </section>
+    );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import Partners from './Partners'
 
 export default function Home() {
   return (
     <main className='w-full'>
       <HeroSection/>
+      <Partners/>
     </main>
   )
 }
